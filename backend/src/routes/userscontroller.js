281f2c33803fa010bfd4860b5b858f4e829c4f19
Carlos Martinez <carlos.martinez@objectwave.com>
2016-08-13 22:45:07 -0500

    Fixes #9

--- backend/src/routes/userscontroller.js
+++ backend/src/routes/userscontroller.js
@@ -5,20 +5,49 @@ var router = express.Router();
 // application logger
 var LOG = require('log4js').getLogger('app');
 var response = require('../dtos/responses');
+var User = require('../models/user');
 
+// constants
 var USERS_PATH = '/users';
+var DEFAULT_REGISTRY_GROUP = 'student';
 
 var routerController = {
 
   /**
-  * Creates a new user.
+  * Registers a new user to the site with basic privileges (student).
   */
   create: function (req, res) {
-    LOG.info('trying to create a user');
-    LOG.info(req.body);
-    console.log(req.body);
-    res.json(new response.Success({fake: 'data'}));
-  },
+    var pass = req.body.password,
+        rPass = req.body.repeatedPassword;
+
+    if (pass !== rPass) {
+      return res.json(new response.Failed('Passwords do not match'));
+    }
+
+    var _newUser = new User({
+      email: req.body.email,
+      name: req.body.name,
+      lastName: req.body.lastName,
+      password: req.body.password,
+      userGroup: DEFAULT_REGISTRY_GROUP,
+      entity: null
+    });
+
+    _newUser.save(function (err, usr) {
+      if (err) {
+        // if this is a douplicate error!
+        if (err.code === 11000) {
+          LOG.warn('User is trying to register an existing account');
+          return res.json(new response.Failed('This email account is already registered, please use the recover password mechanism'));
+        }
+        LOG.error('Could not save new user');
+        LOG.error(err);
+        return res.json(new response.Failed('Could not register the user, please check the information'));
+      }
+
+      return res.json(new response.Success('Thanks for registering to this site, a confirmation email will be sent to your email, please check it out to use your account'));
+    });
+  }
 };
 
 module.exports = function () {
